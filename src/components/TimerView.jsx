35bd20c667ed7645d6a77fc9bb02edd952c1a977
Daniel-GM <danielgomesmoura@gmail.com>
2025-02-04 11:39:38 -0300

    adicionada a função para copiar o /travel

--- src/components/TimerView.jsx
+++ src/components/TimerView.jsx
@@ -1,53 +1,58 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 const TimerView = () => {
-  const INITIAL_TIME = 6003;
-  const [time, setTime] = useState(INITIAL_TIME);
-  const [isRunning, setIsRunning] = useState(false);
-  const [color, setColor] = useState('border-gray-500');
-  const [values, setValues] = useState([0, 0]);
-  const [targetTime, setTargetTime] = useState(null);
+  const INITIAL_TIME = 6003
+  const [time, setTime] = useState(INITIAL_TIME)
+  const [isRunning, setIsRunning] = useState(false)
+  const [color, setColor] = useState('border-gray-500')
+  const [values, setValues] = useState([0, 0])
+  const [targetTime, setTargetTime] = useState(null)
 
   const handleValueChange = (index, newValue) => {
     setValues((prevValues) => {
-      const newValues = [...prevValues];
-      newValues[index] = newValue;
-      return newValues;
-    });
-  };
+      const newValues = [...prevValues]
+      newValues[index] = newValue
+      return newValues
+    })
+  }
+
+  const handleCopyTravel = () => {
+    const textToCopy = values.map(val => val.toString()).join(', ')
+    navigator.clipboard.writeText("/travel " + textToCopy)
+  }
 
   const handleReset = () => {
-    const newTarget = Date.now() + INITIAL_TIME * 10;
-    setTargetTime(newTarget);
-    setTime(INITIAL_TIME);
-    isRunning ? setIsRunning(true) : setIsRunning(false);
-  };
+    const newTarget = Date.now() + INITIAL_TIME * 10
+    setTargetTime(newTarget)
+    setTime(INITIAL_TIME)
+    isRunning ? setIsRunning(true) : setIsRunning(false)
+  }
 
   useEffect(() => {
-    let intervalId;
+    let intervalId
 
     if (isRunning) {
       if (!targetTime) {
-        setTargetTime(Date.now() + time * 10);
+        setTargetTime(Date.now() + time * 10)
       }
 
       intervalId = setInterval(() => {
-        const remaining = Math.ceil((targetTime - Date.now()) / 10);
+        const remaining = Math.ceil((targetTime - Date.now()) / 10)
 
         if (remaining <= 0) {
-          const newTarget = Date.now() + INITIAL_TIME * 10;
-          setTargetTime(newTarget);
-          setTime(INITIAL_TIME);
+          const newTarget = Date.now() + INITIAL_TIME * 10
+          setTargetTime(newTarget)
+          setTime(INITIAL_TIME)
         } else {
-          setTime(remaining);
+          setTime(remaining)
         }
 
         if (remaining <= 800) {
-          setColor('border-red-500');
+          setColor('border-red-500')
         } else {
-          setColor('border-green-500');
+          setColor('border-green-500')
         }
-      }, 30);
+      }, 30)
     } else {
       if (time != INITIAL_TIME) {
         setColor('border-yellow-500')
@@ -56,15 +61,15 @@ const TimerView = () => {
       }
     }
 
-    return () => clearInterval(intervalId);
-  }, [isRunning, targetTime]);
+    return () => clearInterval(intervalId)
+  }, [isRunning, targetTime])
 
   const timer = () => {
-    const minutes = String(Math.floor(time / 6000)).padStart(2, '0');
-    const seconds = String(Math.floor((time % 6000) / 100)).padStart(2, '0');
-    const centiseconds = String(time % 100).padStart(2, '0');
-    return `${minutes}:${seconds}:${centiseconds}`;
-  };
+    const minutes = String(Math.floor(time / 6000)).padStart(2, '0')
+    const seconds = String(Math.floor((time % 6000) / 100)).padStart(2, '0')
+    const centiseconds = String(time % 100).padStart(2, '0')
+    return `${minutes}:${seconds}:${centiseconds}`
+  }
 
   return (
     <div>
@@ -74,11 +79,11 @@ const TimerView = () => {
             <span key={index}>
               <label
                 onClick={() => {
-                  const newValue = prompt('Digite um novo valor:', value);
+                  const newValue = prompt('Digite um novo valor:', value)
                   if (newValue !== null && !isNaN(parseFloat(newValue))) {
-                    handleValueChange(index, parseFloat(newValue));
+                    handleValueChange(index, parseFloat(newValue))
                   } else {
-                    alert('Valor inválido');
+                    alert('Valor inválido')
                   }
                 }}
               >
@@ -99,6 +104,7 @@ const TimerView = () => {
             src="pos.svg"
             alt="position"
             className={`w-7 h-7 bg-gray-300 hover:bg-cyan-400 border-2 ${color} px-1 py-1 rounded-4xl shadow cursor-pointer`}
+            onClick={handleCopyTravel}
           />
         </section>
       </div>
@@ -106,9 +112,9 @@ const TimerView = () => {
         className={`bg-gray-700 hover:bg-gray-600 p-4 rounded-lg shadow border-2 ${color} cursor-pointer`}
         onClick={() => {
           if (!isRunning) {
-            setTargetTime(Date.now() + time * 10);
+            setTargetTime(Date.now() + time * 10)
           }
-          setIsRunning(!isRunning);
+          setIsRunning(!isRunning)
         }}
       >
         <div className="flex items-center justify-center">
@@ -118,7 +124,7 @@ const TimerView = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TimerView;
+export default TimerView
